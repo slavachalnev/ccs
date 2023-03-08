33181ae2007bf731f9dff96a0b225f15662b75a1
Slava Chalnev <slava.chalnev@gmail.com>
2023-03-08 15:08:19 +1100

    Compute features

--- prep_examples.py
+++ prep_examples.py
@@ -1,4 +1,7 @@
 import json
+import torch
+import numpy as np
+
 from transformers import RobertaTokenizer, RobertaModel
 
 
@@ -7,8 +10,8 @@ def get_contrast_pair(q_dict):
     Accepts a question dictionary and returns a contrast pair.
 
     Contrast pair is a tuple of two strings:
-    Passage: [passage]\n\nQuestion: [question]\n\nA: [answer]
-    Passage: [passage]\n\nQuestion: [question]\n\nA: [~answer]
+    Passage: [passage]\n\nQuestion: [question]\n\nA: [True]
+    Passage: [passage]\n\nQuestion: [question]\n\nA: [False]
     """
 
     # Get the passage and question
@@ -18,8 +21,8 @@ def get_contrast_pair(q_dict):
     # Get the correct answer
     answer = q_dict['answer']
 
-    p1 = f"Passage: {passage}\n\nQuestion: {question}\n\nA: {answer}"
-    p2 = f"Passage: {passage}\n\nQuestion: {question}\n\nA: {not answer}"
+    p1 = f"Passage: {passage}\n\nQuestion: {question}\n\nA: True"
+    p2 = f"Passage: {passage}\n\nQuestion: {question}\n\nA: False"
     
     return (p1, p2)
 
@@ -28,8 +31,12 @@ def contrast_features(true_text, false_text, tokenizer, model, layer=10):
     pos_tok = tokenizer(true_text, return_tensors='pt')
     neg_tok = tokenizer(false_text, return_tensors='pt')
 
-    pos_feats = model(pos_tok.input_ids, output_hidden_states=True)['hidden_states'][layer][0][-1]
-    neg_feats = model(neg_tok.input_ids, output_hidden_states=True)['hidden_states'][layer][0][-1]
+    if pos_tok.input_ids.shape[1] > 512:
+        raise ValueError("Input too long!")
+
+    with torch.no_grad():
+        pos_feats = model(pos_tok.input_ids, output_hidden_states=True)['hidden_states'][layer][0][-1]
+        neg_feats = model(neg_tok.input_ids, output_hidden_states=True)['hidden_states'][layer][0][-1]
 
     pos_feats = pos_feats.cpu().detach().numpy()
     neg_feats = neg_feats.cpu().detach().numpy()
@@ -37,20 +44,56 @@ def contrast_features(true_text, false_text, tokenizer, model, layer=10):
     return pos_feats, neg_feats
 
 
-if __name__ == '__main__':
-    data_path = "../../Downloads/boolQ/dev.jsonl"
+def get_all_feats(data_path, tokenizer, model, layer=10, max_num=100):
     # Get the data
     with open(data_path, 'r') as f:
         data = [json.loads(line) for line in f]
 
-    # Get the first question
-    q_dict = data[0]
+    all_feat_pairs = []
+    for idx, q_dict in enumerate(data):
+        if idx > max_num:
+            break
+        # Get the contrast pair
+        true_text, false_text = get_contrast_pair(q_dict)
+        print(f"Processing question {idx}...")
+        # print(len(true_text))
+        # if idx == 55:
+        #     print(true_text)
+        #     print(false_text)
+        try:
+            feats = contrast_features(true_text, false_text, tokenizer, model, layer=layer)
+        except ValueError:
+            print("Input too long!")
+            continue
+        all_feat_pairs.append(feats)
 
-    # Get the contrast pair
-    true_text, false_text = get_contrast_pair(q_dict)
+    return all_feat_pairs
+
+
+if __name__ == '__main__':
+    data_path = "../../Downloads/boolQ/dev.jsonl"
 
     tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
     model = RobertaModel.from_pretrained('roberta-base')
+    model.eval()
 
-    feats = contrast_features(true_text, false_text, tokenizer, model)
-    print(feats)
+    feat_pairs = get_all_feats(data_path, tokenizer, model, layer=10, max_num=100)
+
+    # save as two numpy arrays
+    pos_feats = [pair[0] for pair in feat_pairs]
+    neg_feats = [pair[1] for pair in feat_pairs]
+
+    # ###### normalise ######
+    pos_feats = np.array(pos_feats)
+    neg_feats = np.array(neg_feats)
+
+    # subtract mean
+    pos_feats -= pos_feats.mean(axis=0)
+    neg_feats -= neg_feats.mean(axis=0)
+
+    # divide by std
+    pos_feats /= pos_feats.std(axis=0)
+    neg_feats /= neg_feats.std(axis=0)
+
+    np.save('pos_feats.npy', pos_feats)
+    np.save('neg_feats.npy', neg_feats)
